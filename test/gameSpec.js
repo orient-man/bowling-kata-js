468e857e8b714911325cd4cd50fe114085466ea0
Marcin Malinowski <orientman@gmail.com>
2013-05-25 17:03:14 +0200

    Refactor tests to make them more DRY

--- test/gameSpec.js
+++ test/gameSpec.js
@@ -2,16 +2,22 @@
 
 describe("Ten-Ping Bowling Kata", function () {
 
+    function rollMany(rolls, pins) {
+        var i = 0;
+        for (i; i < rolls; i++) {
+            this.roll(pins);
+        }
+    }
+
+    beforeEach(function () {
+        this.game = new Game();
+    });
+
     describe("Gutter Game", function () {
 
         it("should score 0 for a gutter game, all rolls are 0", function () {
-            var game = new Game(), i = 0;
-
-            for (i; i < 20; i++) {
-                game.roll(0);
-            }
-
-            expect(game.score()).toEqual(0);
+            rollMany.call(this.game, 20, 0);
+            expect(this.game.score()).toEqual(0);
         });
 
     });
@@ -19,13 +25,8 @@ describe("Ten-Ping Bowling Kata", function () {
     describe("Score game given all rolls hit only one pin", function () {
 
         it("should score 20", function () {
-            var game = new Game(), i = 0;
-
-            for (i; i < 20; i ++) {
-                game.roll(1);
-            }
-
-            expect(game.score()).toEqual(20);
+            rollMany.call(this.game, 20, 1);
+            expect(this.game.score()).toEqual(20);
         });
 
     });
