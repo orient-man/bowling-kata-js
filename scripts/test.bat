59ae0d896437da657dd92a3bd2877d2ae57881e6
malin <malin@piatka.pl>
2013-05-27 10:35:53 +0200

    Karma script for Windows

---
+++ scripts/test.bat
@@ -0,0 +1,12 @@
+@echo off
+
+REM Windows script for running unit tests
+REM You have to run server and capture some browser first
+REM
+REM Requirements:
+REM - NodeJS (http://nodejs.org/)
+REM - Testacular (npm install -g testacular)
+
+set BASE_DIR=%~dp0
+set PHANTOMJS_BIN=c:/PhantomJS/phantomjs.exe
+karma start "%BASE_DIR%\..\config\karma.conf.js" %*
